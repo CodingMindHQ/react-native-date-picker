01130c76bfff58e57a3002a972b50a6ab853a79a
andy-io <10921951+andy-io@users.noreply.github.com>
2021-09-05 20:32:09 +0200

    Update AndroidNative.java

--- android/src/main/java/com/henninghall/date_picker/pickers/AndroidNative.java
+++ android/src/main/java/com/henninghall/date_picker/pickers/AndroidNative.java
@@ -54,7 +54,6 @@ public class AndroidNative extends NumberPicker implements Picker {
         int color = Color.parseColor(stringColor);
 
         if (Build.VERSION.SDK_INT >= 29) {
-            super.setTextColor(color);
             return;
         }
 
